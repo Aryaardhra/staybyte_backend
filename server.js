00766275e15d23abf044d9f5a0cd1a7d873cd9e4
Aryaardhra <ardhra455@gmail.com>
2026-02-10 01:37:53 +0530

    cors origin

--- server.js
+++ server.js
@@ -27,13 +27,18 @@ const PORT = process.env.PORT || 3002;
 connectDB();
 await connectCloudinary();
 
-const allowedOrigins = [ "https://staybyte-frontend-3jy4bf2b9-ardhras-projects-261e273a.vercel.app" || "http://localhost:5173" ]
+//const allowedOrigins = [ "https://staybyte-frontend-3jy4bf2b9-ardhras-projects-261e273a.vercel.app" || "http://localhost:5173" ]
 
 
 
 //middlewares
 // ✅ CORS setup
-app.use(cors({ origin: allowedOrigins, credentials: true }));
+app.use(cors({
+    origin: "https://staybyte-frontend-3jy4bf2b9-ardhras-projects-261e273a.vercel.app",
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+}));
 
 app.use(clerkMiddleware());
 
